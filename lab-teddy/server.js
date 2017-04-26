25b3d7c2af3577b8028730e443266310120f4bde
David Teddy <davidteddy@outlook.com>
2017-04-26 10:31:22 -0700

    got everthing written now debugging

---
+++ lab-teddy/server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const bodyParser = require('./lib/body-parser');
+const http = require('http');
+const url = require('url');
+const queryString = require('queryString');
+const cowsay = require('cowsay');
+const PORT = process.env.PORT || 8080;
+
+const server = module.exports = http.createServer(function(req, res){
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
+  if(req.method === 'POST'){
+    if(req.url.pathname === '/cowsay'){
+      bodyParser(req, function(err){
+        if(err)console.error(err);
+        let message = cowsay.say({test: req.body.text});
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(message);
+        res.end();
+      });
+    } else{
+      let message = cowsay.say(
+        {text:'bad request\ntry localhost:8080/cowsay with a proper body'}
+      );
+      res.writeHead(400, {'Content-Type':'text/plain'});
+      res.write(message);
+      res.end();
+    }
+  }
+  if(req.method === 'GET'){
+    if(req.url.pathname === '/cowsay'){
+      let message = cowsay.say({test: req.url.query.text});
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(message);
+      res.end();
+    } else {
+      let message = cowsay.say({text: 'bad request\ntry localhost:8080/cowsay with a proper body'});
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.message(message);
+      res.end();
+    }
+  }
+});
+
+server.listen(PORT, () => console.log(`Listening on port, ${PORT}`));
